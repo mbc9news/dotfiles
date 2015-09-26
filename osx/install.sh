acf833f64afae1e18986b5022fa0817a55705250
Choi ByungHoon <mbc9news@gmail.com>
2015-09-27 01:03:33 +0900

    Update install.sh

--- osx/install.sh
+++ osx/install.sh
@@ -4,7 +4,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 
 PROG_BASE='grc coreutils tree zsh pkg-config libevent openssl pcre xz gettext git bash maven go docker scala pivotal/tap/springboot caskroom/cask/brew-cask node'
-PROG_CASK='alfred dropbox go2shell intellij-idea keepingyouawake slack the-unarchiver vox atom evernote google-chrome iterm2 python3 sublime-text3 virtualbox devonthink-pro github-desktop haskell-platform java scrivener telegram visualvm duet'
+PROG_CASK='alfred dropbox go2shell intellij-idea keepingyouawake slack the-unarchiver vox atom evernote google-chrome iterm2 python3 sublime-text3 virtualbox devonthink-pro github-desktop haskell-platform java scrivener telegram visualvm duet utorrent'
 
 brew install ${PROG_BASE}
 brew tap caskroom/versions
