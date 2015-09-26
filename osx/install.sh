2762f85f6ce03c5b3710325ce9ffd585bcc14813
Choi ByungHoon <mbc9news@gmail.com>
2015-09-26 18:02:58 +0900

    Update install.sh

--- osx/install.sh
+++ osx/install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 PROG_BASE='grc coreutils tree zsh pkg-config libevent openssl pcre xz gettext git bash maven go docker scala pivotal/tap/springboot caskroom/cask/brew-cask node'
-PROG_CASK='google-chrome java intellij-idea visualvm sublime-text python3 atom evernote scrivener dropbox virtualbox alfred devonthink-pro github-desktop haskell-platform keepingyouawake'
+PROG_CASK='google-chrome java intellij-idea python3 atom evernote scrivener dropbox virtualbox alfred devonthink-pro github-desktop haskell-platform keepingyouawake'
 
 brew install ${PROG_BASE}
+brew tap caskroom/versions
+brew cask install sublime-text3
 brew cask install ${PROG_PROG}
-
